b9dc15c4d605eeda7ec83f2f7b8b324c82019af0
Wojciech Rzepliński <wojtekrzeplinski@gmail.com>
2020-07-18 13:07:45 +0200

    phonebook continue

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import ShowPersons from './components/ShowPersons'
 
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('rendering', persons.length, 'persons')
 
   const personsToShow = (filter.length > 0 ?
     persons.filter((person) => (
